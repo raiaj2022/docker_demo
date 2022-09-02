33cf4b0bb94a8e6d496f31ba93857e009a4ebf62
Ajaya Rai <ajaya.rai@centrica.com>
2022-09-02 14:30:13 +0100

    set up backend

---
+++ backend/app.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const cors = require('cors')
+
+const app = express()
+
+app.use(cors())
+
+app.get('/', (req, res) => {
+  res.json([
+    {
+      "id": "1",
+      "name": "John Doe",
+      "gender": "male",
+      "age": 38
+    }
+  ])
+})
+
+app.listen(4000, () => {
+  console.log('listening for requests on port 4000')
+})
